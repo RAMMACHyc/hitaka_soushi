c77a26b8dd2945a44603819f0ae0118bf9aef34b
RAMMACHyc <sadik.ram12@gmail.com>
2022-11-16 19:21:22 +0100

    modified

--- main.js
+++ main.js
@@ -35,8 +35,6 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
     cart.classList.remove("active");
 };
-
-
 //
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', start);
@@ -44,83 +42,36 @@ if (document.readyState == 'loading') {
     start();
 }
 
-// Moking Function
-function start() {
-    addEvents();
-}
 
-function update() {
-    addEvents();
-    updateTotal();
-}
+// FUNCTIONS
 
-function addEvents() {
-    let cartRemove_btns = document.querySelectorAll('.cart-remove');
-    console.log(cartRemove_btns);
-    cartRemove_btns.forEach((btn) => {
-        btn.addEventListener("click", handle_removerCartItems());
-
-    });
-
-    // change items quantiy
-    let cartQuantity_inputs = document.querySelectorAll('.cart-quantity');
-    cartQuantity_inputs.forEach(input => {
-        input.addEventListener("change", handle_changeItemQuantity);
-    });
-    //Add item to cart
-    let addCart_btns = document.querySelectorAll(".shoping");
-    addCart_btns.forEach(btn => {
-        btn.addEventListener("click", handle_addCartItem);
-    });
-}
-//HANDLE EVENTS FUNCTIONS
-function handle_addCartItem() {
-    let product = this.parentElement;
-    let title = product.querySelector(".product-title").innerHTML;
-    let price = product.querySelector(".prix").innerHTML;
-
-    let cartBoxElement;
-    let newNode = document.createElement("div");
-
-    const cartContent = cart.querySelector(".cart-content");
-    cartContent.appendChild(newNode);
-    document.getElementById("bi").innerHTML += `
-    <div class = "cart-box ">
-       <img src = "" alt = " " class = "cart-img ">
-     <div class = "detail-box ">
-       <div class = "cart-product-title ">${title}</div>
-        <div class = "cart-price " >${price}</div>
-         <input type = "number" value = "1" class = "cart-quantity ">
-     </div>
-        <i class = 'bx bxs-trash-alt cart-remove' ></i>
-   </div> `
-}
-
-function handle_removerCartItems() {
-    this.parentElement.remove();
-
-    update();
-}
-
-function handle_changeItemQuantity() {
-    if (isNaN(this.value) || this.value < 1) {
-        this.value = 1;
+function addtocart(id) {
+    var element = document.getElementsByClassName('dash-card')[id];
+    var image = element.getElementsByClassName('card-image')[0].src;
+    var spliteImg = image.split('hitaka_soushi/');
+    image = spliteImg[1];
+    var div = element.getElementsByClassName('product-title')[0].innerHTML;
+    var myArray = div.split(" <");
+    var nameProduct = myArray[0];
+    div = element.getElementsByClassName('product-title')[0];
+    var prix = div.getElementsByClassName('prix')[0].innerHTML;
+    var product = {
+        'imageSrc': image,
+        'name': nameProduct,
+        'prix': prix
     }
-    this.value = Math.floor(this.value);
-
-    update();
-}
-
-function updateTotal() {
-    let cartBoxes = document.querySelectorAll('.cart-box');
-    const totalElement = cart.querySelector('.total-price');
-    let total = 0;
-    cartBoxes.forEach(cartBox => {
-        let priceElement = cartBox.querySelector('.cart-price');
-        let price = parseFloat(priceElement.innerHTML.replace("DH", ""));
-        let quantity = cartBox.querySelector(".cart-quantity").value;
-        total += price * quantity;
-    });
-    total = total.toFixed(2);
-    totalElement.innerHTML = "DH" + total;
+    var child = document.createElement('div');
+    child.className = 'cart-box';
+    var divProduct = `
+                <img src="./${product.imageSrc}" alt="" class="cart-img ">
+                <div class="detail-box ">
+                    <div class="cart-product-title ">${product.name}</div>
+                    <div class="cart-price ">${product.prix}</div>
+                    <input type="number" value="1" class="cart-quantity ">
+                </div>
+                <i class='bx bxs-trash-alt cart-remove'></i>
+    `;
+    child.innerHTML = divProduct;
+    var parent = document.getElementsByClassName("cart-content")[0];
+    parent.appendChild(child);
 }
